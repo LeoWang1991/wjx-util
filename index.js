8f8aca3aafe4e0b4fe12f3309c93f5d193e334a5
xiaofei <kingswang007@qq.com>
2022-09-07 10:28:06 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 function formatTime(time, format = "YYYY-MM-DD") {
-const a = "xiaofei";
+  const isDate = time instanceof Date
+  const date = isDate ? time : new Date(time)
+  if(isNaN(date.valueOf())) {
+    return '--'
+  }
+
+  const addZero = value => value < 10 ? `0${value}` : value
+
+  const y = date.getFullYear()
+  const m = date.getMonth() + 1
+  const d = date.getDate()
+
+  let dateStr = `${y}-${addZero(m)}-${addZero(d)}`
+
+  if(format.includes('HH')) {
+    const h = date.getHours()
+    const m = date.getMinutes()
+    const s = date.getSeconds()
+    dateStr += ` ${addZero(h)}:${addZero(m)}:${addZero(s)}`
+  }
+
+  return dateStr
 }
+
